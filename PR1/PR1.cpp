b188e554537f68c11f40963262f1d653c151a32c
a2468834 <a2468834@gmail.com>
2018-04-09 19:22:50 +0800

    Finish the part of A* (with teacher-provided heuristic function).

--- PR1/PR1.cpp
+++ PR1/PR1.cpp
@@ -1,14 +1,14 @@
-/* test about newNode and addChild!!!
- */
-
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <cmath>
 #include <iostream>
 #include <string>
 #include <deque>
+#include <map>
 #include <vector>
 #include <utility>
+#include <iomanip>
 
 using namespace std;
 
@@ -30,14 +30,14 @@ struct node
 };
 
 // function pre-declarations
-size_t split(const string &input, vector<string> &output, char delimiter);
-struct node* newNode(pair<int, int> coord, pair<char, int> action);
-void printSolution(struct node* root_node);
-void BFS(pair<int, int> goal, vector<int> move);
-void IDS(pair<int, int> goal, vector<int> move);
-struct node* DLS(pair<int, int> goal, vector<int> move, int limit);
-struct node* Rec_DLS(struct node* current_node, pair<int, int> goal, vector<int> move, int limit);
-void A_Star();
+size_t split( const string &input, vector<string> &output, char delimiter );
+struct node* newNode( pair<int, int> coord, pair<char, int> action );
+void printSolution( struct node* root_node );
+int pathCost( int next_action, vector<int> move );
+int heuriFunc( pair<int, int> current, pair<int, int> goal );
+void BFS( pair<int, int> goal, vector<int> move );
+void IDS( pair<int, int> goal, vector<int> move );
+void A_Star( pair<int, int> goal, vector<int> move );
 
 int main()
 {
@@ -81,7 +81,12 @@ int main()
             cout<<"--------------------"<<endl;
             IDS( goal_point, move );
         }
-        else if(split_str[0] == "A*");
+        else if(split_str[0] == "A*")
+        {
+            cout<<getLine_str;
+            cout<<"--------------------"<<endl;
+            A_Star( goal_point, move );
+        }
         else error_method = -1;
 
         if( error_method == -1 )
@@ -95,7 +100,7 @@ int main()
     return 0;
 }
 
-size_t split(const string &input, vector<string> &output, char delimiter)
+size_t split( const string &input, vector<string> &output, char delimiter )
 {
     size_t pos = input.find( delimiter );
     size_t initialPos = 0;
@@ -141,20 +146,63 @@ void printSolution( struct node* goal_node, struct node* root_node)
     struct node* itr = goal_node;
     while(true)
     {
-        cout<<itr->coord.first<<" "<<itr->coord.second<<" ";
-        cout<<itr->action.first<<" "<<itr->action.second<<"@@";
-        cout<<itr->next_action<<endl;
+        cout<<"Through:("<<itr->action.first<<", "<<itr->action.second<<")";
+        cout<<", To:("<<itr->coord.first<<", "<<itr->coord.second<<")"<<endl;
         itr = itr->parent;
         if(itr == root_node)break;
     }
 
-    cout<<itr->coord.first<<" "<<itr->coord.second<<" ";
-    cout<<itr->action.first<<" "<<itr->action.second<<"@@";
-    cout<<itr->next_action<<endl;
+    cout<<"Through:("<<itr->action.first<<", "<<itr->action.second<<")";
+    cout<<", To:("<<itr->coord.first<<", "<<itr->coord.second<<")"<<endl;
     cout<<"===================="<<endl;
     cout<<endl;
     
     if(root_node->parent != root_node)cout<<"Error!";
+}
+
+// print node with path cost
+void printSolWithEvaFunc( struct node* goal_node, struct node* root_node, vector<int> move )
+{
+    struct node* itr = goal_node;
+    while(true)
+    {
+        cout<<"Through:("<<itr->action.first<<", "<<itr->action.second<<")";
+        cout<<", To:("<<itr->coord.first<<", "<<itr->coord.second<<")";
+        cout<<", Evaluation:";
+        cout<<heuriFunc(itr->coord, goal_node->coord)<<" + ";
+        cout<<pathCost(itr->next_action, move)<<endl;
+        itr = itr->parent;
+        if(itr == root_node)break;
+    }
+
+    cout<<"Through:("<<itr->action.first<<", "<<itr->action.second<<")";
+    cout<<", To:("<<itr->coord.first<<", "<<itr->coord.second<<")";
+    cout<<", Evaluation:";
+    cout<<heuriFunc(itr->coord, goal_node->coord)<<" + ";
+    cout<<pathCost(itr->next_action, move)<<endl;
+    
+    if(root_node->parent != root_node)cout<<"Error!";
+}
+
+int heuriFunc( pair<int, int> current, pair<int, int> goal )
+{
+    int first_comp = 0;
+    int second_comp = 0;
+
+    first_comp = (int) floor(abs(current.first - goal.first)/9);
+    second_comp = (int) floor(abs(current.second - goal.second)/9);
+
+    return (first_comp + second_comp);
+}
+
+int pathCost( int next_action, vector<int> move )
+{
+    int result = 0;
+    for(int i=0; i<next_action; i++)
+    {
+        result = result + move[i];
+    }
+    return result;
 }
 
 void BFS( pair<int, int> goal, vector<int> move )
@@ -167,9 +215,8 @@ void BFS( pair<int, int> goal, vector<int> move )
     frontier.push_back( root_node );
 
     struct node* temp_node = NULL;
-    do{
-        if( frontier.empty() == true )return;
-
+    while(frontier.empty() != true)
+    {
         explored_node = frontier.front();
         frontier.pop_front();
 
@@ -217,7 +264,7 @@ void BFS( pair<int, int> goal, vector<int> move )
             explored_node->next_action+1
         ); // stop
         frontier.push_back(temp_node);
-    }while( explored_node->coord != goal );
+    }
 
     printSolution(explored_node, root_node);
 }
@@ -299,62 +346,69 @@ void IDS( pair<int, int> goal, vector<int> move )
         }
     }
 }
-/*
-struct node* Rec_DLS( struct node* root_node, pair<int, int> goal, vector<int> move, int limit )
+
+void A_Star( pair<int, int> goal, vector<int> move )
 {
-    if(current_node->coord == goal)return current_node;
-    else if(limit == 0)return NULL;// cutoff
-    else
+    multimap<int, struct node*>frontier;
+    struct node* root_node = newNode( NULL, make_pair(0, 0), make_pair('s', 0), 0 );
+    struct node* explored_node;
+    multimap<int, struct node*>::iterator itr;
+
+    root_node->parent = root_node;
+    frontier.insert( make_pair( ( heuriFunc(root_node->coord, goal) + pathCost(root_node->next_action, move) ), root_node ) );
+
+    struct node* temp_node = NULL;
+    while( frontier.empty() != true )
     {
-        int change = move[current_node->next_action];
-        struct node* temp_node = NULL;
-        struct node* cutoff_node = NULL;
-        // if 'cutoff_node' is not null, it must be the solution node!
+        itr = frontier.begin();
+        explored_node = itr->second;
+        frontier.erase(itr);
+
+        int change = move[explored_node->next_action];
+
+        if( explored_node->coord == goal )break;
+        // find the solution and stop BFS tree search
 
         temp_node = newNode(
-            current_node, 
-            make_pair(current_node->coord.first+change, current_node->coord.second), 
+            explored_node, 
+            make_pair(explored_node->coord.first+change, explored_node->coord.second), 
             make_pair('x', +change), 
-            current_node->next_action+1
+            explored_node->next_action+1
         ); // x+
-        cutoff_node = Rec_DLS(temp_node, goal, move, limit-1);
-        if(cutoff_node != NULL)return cutoff_node;
+        frontier.insert( make_pair( ( heuriFunc(temp_node->coord, goal) + pathCost(temp_node->next_action, move) ), temp_node ) );
 
         temp_node = newNode(
-            current_node, 
-            make_pair(current_node->coord.first-change, current_node->coord.second), 
+            explored_node, 
+            make_pair(explored_node->coord.first-change, explored_node->coord.second), 
             make_pair('x', -change), 
-            current_node->next_action+1
+            explored_node->next_action+1
         ); // x-
-        cutoff_node = Rec_DLS(temp_node, goal, move, limit-1);
-        if(cutoff_node != NULL)return cutoff_node;
+        frontier.insert( make_pair( ( heuriFunc(temp_node->coord, goal) + pathCost(temp_node->next_action, move) ), temp_node ) );
 
         temp_node = newNode(
-            current_node, 
-            make_pair(current_node->coord.first, current_node->coord.second+change), 
+            explored_node, 
+            make_pair(explored_node->coord.first, explored_node->coord.second+change), 
             make_pair('y', +change), 
-            current_node->next_action+1
+            explored_node->next_action+1
         ); // y+
-        cutoff_node = Rec_DLS(temp_node, goal, move, limit-1);
-        if(cutoff_node != NULL)return cutoff_node;
+        frontier.insert( make_pair( ( heuriFunc(temp_node->coord, goal) + pathCost(temp_node->next_action, move) ), temp_node ) );
 
         temp_node = newNode(
-            current_node, 
-            make_pair(current_node->coord.first, current_node->coord.second-change), 
+            explored_node, 
+            make_pair(explored_node->coord.first, explored_node->coord.second-change), 
             make_pair('y', -change), 
-            current_node->next_action+1
+            explored_node->next_action+1
         ); // y-
-        cutoff_node = Rec_DLS(temp_node, goal, move, limit-1);
-        if(cutoff_node != NULL)return cutoff_node;
+        frontier.insert( make_pair( ( heuriFunc(temp_node->coord, goal) + pathCost(temp_node->next_action, move) ), temp_node ) );
 
         temp_node = newNode(
-            current_node, 
-            make_pair(current_node->coord.first, current_node->coord.second), 
+            explored_node, 
+            make_pair(explored_node->coord.first, explored_node->coord.second), 
             make_pair('s', 0), 
-            current_node->next_action+1
+            explored_node->next_action+1
         ); // stop
-        cutoff_node = Rec_DLS(temp_node, goal, move, limit-1);
-        if(cutoff_node != NULL)return cutoff_node;
+        frontier.insert( make_pair( ( heuriFunc(temp_node->coord, goal) + pathCost(temp_node->next_action, move) ), temp_node ) );
     }
+
+    if( frontier.empty() != true)printSolWithEvaFunc(explored_node, root_node, move);
 }
-*/
